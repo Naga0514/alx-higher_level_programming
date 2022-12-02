07ef968210e50e314c914936278fc56aa4368bf3
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-02 23:28:19 +0100

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number:d} Battery street")
+from sys import argv
+if __name__ == "__main__":
+    sum = 0
+    for i in range(1, len(argv)):
+        sum = sum + int(argv[i])
+    print("{:d}".format(sum))
