549d3264781d81a026621fab9ec8b48f516acf1d
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-05 13:23:57 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
-        return None
-    return my_list[idx]
+def divisible_by_2(my_list=[]):
+    if my_list is not None:
+        new = list(my_list)
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                new[i] = True
+            else:
+                new[i] = False
+        return new
