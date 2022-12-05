d6b9907a586f148126a4cd221f8b5178ba465881
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-05 13:15:56 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,9 @@
-ysyfufuy
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    copy = list(my_list)
+    copy[idx] = element
+    return copy
