fd9e0f43a8e879e23107512ce8111aa40ab1db12
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-05 13:18:36 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,9 @@
-vghvhvhj
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tup = ()
+    sent = list(sentence)
+    if sentence == "":
+        tup = (len(sent), None)
+    else:
+        tup = (len(sent), sent[0])
+    return tup
