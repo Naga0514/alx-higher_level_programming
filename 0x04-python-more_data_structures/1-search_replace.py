d127aa6f9392bc45f96a4058caa09195ba81d1b2
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-08 12:07:53 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,6 @@
-hfhvh`
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    dup_list = my_list.copy()
+    for i in range(len(dup_list)):
+        dup_list[i] = replace if my_list[i] == search else my_list[i]
+    return (dup_list)
