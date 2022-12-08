2ec016d6e6d92403806b80aa8cfdb3c32686596f
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-08 12:13:53 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
-vvdgvhvv
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not (a_dictionary):
+        return (None)
+    best = max(a_dictionary.values())
+    for key in a_dictionary.keys():
+        if (a_dictionary[key] == best):
+            return (key)
