9cf3087d4e50c0e0d78e27e4535ed2ffb6d6e8c6
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-12-19 07:42:32 +0100

    Update 5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,1 +1,8 @@
- vdbvdhvh
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
